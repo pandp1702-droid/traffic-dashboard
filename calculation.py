630c91d887d0b1c9c693b56a62b73c3cb616796d
pandp1702-droid <pandp.1702@gmail.com>
2026-08-13 15:13:04 +0700

    Update calculation.py

--- calculation.py
+++ calculation.py
@@ -37,13 +37,8 @@ def calculate(df):
     # =====================================
 
     if "Need" in df.columns:
-
-        df["Outstanding"] = (
-            df["Need"]
-        )
-
+        df["Outstanding"] = df["Need"]
     else:
-
         df["Outstanding"] = 0
 
     # =====================================
@@ -51,39 +46,26 @@ def calculate(df):
     # =====================================
 
     inventory_columns = [
-
         "Other+",
-
         "Sample+",
-
         "Test+",
-
         "P&O WP",
-
         "OSP+",
-
         "Suspend+",
-
         "Rdy Shp+"
     ]
 
     existing_inventory = [
-
         col
-
         for col in inventory_columns
-
         if col in df.columns
     ]
 
     if len(existing_inventory) > 0:
 
         df["Coil_Inv"] = (
-
             df[existing_inventory]
-
             .sum(axis=1)
-
         )
 
     else:
@@ -95,43 +77,11 @@ def calculate(df):
     # =====================================
 
     df["Production_Add"] = np.maximum(
-
         0,
-
         df["Outstanding"]
-
         - df["Coil_Inv"]
-
     )
 
     # =====================================
     # REMAINING COIL
-    # =====================================
-
-    df["Remaining_Coil"] = np.maximum(
-
-        0,
-
-        df["Coil_Inv"]
-
-        - df["Outstanding"]
-
-    )
-
-    # =====================================
-    # MOVE COIL
-    # =====================================
-
-    if "Rdy Shp+" in df.columns:
-
-        df["Move_Coil"] = (
-
-            df["Rdy Shp+"]
-
-        )
-
-    else:
-
-        df["Move_Coil"] = 0
-
-    # ============================
+    # ========================
